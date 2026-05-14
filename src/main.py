b5ded9965f9de29001207caf813e3c32c8dae0ee
Hugolopez5134 <victor.lmartin@alumnos.udg.mx>
2026-05-14 12:22:18 -0600

    feat: add initial mlops pipeline structure

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
-if __name__ == "__main__": pass
+import yaml
+
+
+def load_config():
+    with open("config/params.yaml", "r") as file:
+        return yaml.safe_load(file)
+
+
+def main():
+    config = load_config()
+
+    print("Configuración cargada correctamente")
+    print(config)
+
+
+if __name__ == "__main__":
+    main()
